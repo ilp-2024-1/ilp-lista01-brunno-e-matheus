99884ddae6e2a0c3c4594712715c1951d8e128ee
Brunno2m <brunno.m@ifrn.edu.br>
2024-07-17 11:23:20 -0300

    Atividade ate 6 feita

--- atv_1.py
+++ atv_1.py
@@ -26,7 +26,50 @@
 #3. Área do Círculo: Solicite ao usuário um valor do raio de um círculo, calcule sua
 #área e exiba o resultado do cálculo.
 
-r = int(input('Digite o valor do raio do círculo: '))
-A = 3.14*r*r
-print('A área do circulo é:',A)
+# r = int(input('Digite o valor do raio do círculo: '))
+# A = 3.14*r*r
+# print('A área do circulo é:',A)
+
+# 4. Área do Triângulo: Solicite ao usuário um valor da base e da altura de um
+# triângulo, calcule sua área e exiba o resultado do cálculo.
+
+# print('Calculadora da área do triângulo')
+# base = int(input('Insira o valor da base: '))
+# alt = int(input('Insira o valor da altura: '))
+
+# area = (base*alt)/2
+
+# print('A área do triangulo é: ',area)
+# print('Fim do programa')
+
+# 5. Volume da Esfera: Solicite ao usuário um valor do raio de uma esfera, calcule seu
+# volume e exiba o resultado do cálculo.
+
+# print('Calcular Volume da Esfera')
+
+# r = int(input('Digite o valor do raio da esfera: '))
+# import math
+
+# v = (4/3)*math.pi*r**3
+
+# print('O volume total da esfera é: ',v)
+# print('Fim do programa')
+
+# 6. Calculadora de Média Aritmética: Solicite ao usuário para que ele insira três
+# valores de notas, realize o cálculo da média aritmética e em seguida exiba os três
+# valores digitados pelo usuário e o resultado do cálculo.
+
+# print('Média aritimética')
+# v1 = int(input('Insira nota 1: '))
+# v2 = int(input('Insira nota 2: '))
+# v3 = int(input('Insira nota 3: '))
+
+# media = (v1+v2+v3)/3
+
+# print('A média aritimética de ', v1 ,'+', v2 ,'+', v3 , 'é: ',media)
+# print('Fim do programa')
+
+# 7. Calculadora de Média Ponderada: Solicite ao usuário para que ele insira os
+# valores de 4 notas e seus respectivos pesos, em seguida realize o cálculo da média
+# pondera e exiba o resultado do cálculo.
 
